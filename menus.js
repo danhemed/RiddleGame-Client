55ca1dbc3d623866e0a59a884b24491ce43a0cf9
Dan <danhemed123@gmail.com>
2025-07-20 12:39:20 +0300

    .

--- menus.js
+++ menus.js
@@ -2,7 +2,7 @@ import readline from "readline-sync";
 
 export function Menu() {
     return readline.question(
-    "What do you want to do?\n" +
+    "\nWhat do you want to do?\n" +
     "1. Play the game >>\n" +
     "2. Create a new riddle >>\n" +
     "3. Read all riddles >>\n" +
